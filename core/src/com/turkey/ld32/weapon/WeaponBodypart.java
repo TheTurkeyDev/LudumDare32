8945bc7e7dceec962bb83dbacc445f490cf97d88
turkey2349 <Turkey2349@gmail.com>
2015-09-11 09:09:18 -0400

    - Fixed an animation glitch with the severed arm

--- core/src/com/turkey/ld32/weapon/WeaponBodypart.java
+++ core/src/com/turkey/ld32/weapon/WeaponBodypart.java
@@ -21,41 +21,39 @@ public class WeaponBodypart extends Weapon
 	}
 
 	public void render()
-	{	
-		if(!isActive)
-			return;
+	{
+		if(!isActive) return;
 
-		stage+=3;
+		stage += 5;
+		if(stage >= 90)
 		{
-			stage=0;
 			this.isActive = false;
-			for(Entity entity: super.holder.getRoom().getEntites())
+			for(Entity entity : super.holder.getRoom().getEntites())
 			{
-				float xDist = entity.getX()-super.holder.getX();
-				float xDist2 = (entity.getX()+entity.getWidth())-super.holder.getX();
-				if(Math.abs(xDist)>Math.abs(xDist2))
-					xDist=xDist2;
-				float yDist = entity.getY()-super.holder.getY();
-				float yDist2 = (entity.getY()-entity.getHeight())-super.holder.getY();
-				if(Math.abs(yDist)>Math.abs(yDist2))
-					yDist=yDist2;
+				float xDist = entity.getX() - super.holder.getX();
+				float xDist2 = (entity.getX() + entity.getWidth()) - super.holder.getX();
+				if(Math.abs(xDist) > Math.abs(xDist2)) xDist = xDist2;
+				float yDist = entity.getY() - super.holder.getY();
+				float yDist2 = (entity.getY() - entity.getHeight()) - super.holder.getY();
+				if(Math.abs(yDist) > Math.abs(yDist2)) yDist = yDist2;
 
-				double angle2 = Math.toDegrees(Math.atan2(yDist,xDist));
+				double angle2 = Math.toDegrees(Math.atan2(yDist, xDist));
 
 				double dist = Math.sqrt(Math.pow(yDist, 2) + Math.pow(xDist, 2));
-				
+
 				if(Math.abs(angle2 - this.angle) < 25 && dist < 65)
 				{
 					entity.damage(this.damage);
 					break;
 				}
 			}
+			stage = 0;
 		}
 
-		int x = (int) Math.floor((Math.cos(Math.toRadians(angle+stage-30))*(30)));
-		int y =  (int) Math.floor((int) (Math.sin(Math.toRadians(angle+stage-30))*(30)));
+		int x = (int) Math.floor((Math.cos(Math.toRadians(angle + stage - 30)) * (30)));
+		int y = (int) Math.floor((int) (Math.sin(Math.toRadians(angle + stage - 30)) * (30)));
 
-		Draw2D.drawTextured(super.holder.getX()+x, super.holder.getY()+y, texture, angle-90);
+		Draw2D.drawTextured(super.holder.getX() + x, super.holder.getY() + y, texture, angle - 90);
 	}
 
 	public void onActivate()
